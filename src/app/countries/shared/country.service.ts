b48d39f06b8cc8d92de2173f364c629be4d082ca
Alexander Agyapong <30531859+alexAgyapong@users.noreply.github.com>
2020-06-04 20:01:26 +0100

    Add params for any number of codes in the request

--- src/app/countries/shared/country.service.ts
+++ src/app/countries/shared/country.service.ts
@@ -1,6 +1,6 @@
 import { Country } from './country';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { tap, map } from 'rxjs/operators';
 import { StorageMap } from '@ngx-pwa/local-storage';
@@ -30,17 +30,20 @@ export class CountryService {
         tap(data => console.log('country details', data))
       );
   }
+
   getBorderCountry(codes: string[]): Observable<any[]> {
-    console.log('codes', codes);
-    const url = `${this.baseUrl}/alpha?codes=${codes[0]};${codes[1]};${codes[2]}`;
-    return this.http.get<Country[]>(url)
+    let options = new HttpParams();
+    options = options.append('codes', codes.join(';'));
+    const url = `${this.baseUrl}/alpha`;
+
+    return this.http.get<Country[]>(url, { params: options })
       .pipe(
         map((res) => {
           return res.map((country) => {
             return {
               code: country.alpha3Code,
               name: country.name
-            }
+            };
           }
           );
         }), tap(data => console.log('border country names details', data)));
